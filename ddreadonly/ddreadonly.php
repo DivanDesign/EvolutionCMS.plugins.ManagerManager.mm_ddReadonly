a4245896092a7bec50a7883f245ef862969bc55e
Ilyas Ronef <ilyas@divandesign.biz>
2016-11-20 15:47:31 +0500

    * Minor changes.

--- ddreadonly/ddreadonly.php
+++ ddreadonly/ddreadonly.php
@@ -5,18 +5,27 @@
  * 
  * @desc A widget for ManagerManager allowing read-only mode for fields and TVs (their values are still visible but can not be changed).
  * 
- * @uses ManagerManager plugin 0.6.2.
+ * @uses MODXEvo.plugin.ManagerManager >= 0.6.2.
  * 
- * @param $fields {comma separated string} - The name(s) of the document fields (or TVs) for which the widget is applying. @required
- * @param $roles {comma separated string} - The roles that the widget is applied to (when this parameter is empty then widget is applied to the all roles). Default: ''.
- * @param $templates {comma separated string} - Templates IDs for which the widget is applying (empty value means the widget is applying to all templates). Default: ''.
+ * @param $fields {string_commaSeparated} — The name(s) of the document fields (or TVs) for which the widget is applying. @required
+ * @param $roles {string_commaSeparated} — The roles that the widget is applied to (when this parameter is empty then widget is applied to the all roles). Default: ''.
+ * @param $templates {string_commaSeparated} — Templates IDs for which the widget is applying (empty value means the widget is applying to all templates). Default: ''.
+ * 
+ * @event OnBeforeDocFormSave
+ * @event OnDocFormSave
+ * @event OnDocDuplicate
+ * @event OnDocFormRender
  * 
  * @link http://code.divandesign.biz/modx/mm_ddreadonly/1.0.2
  * 
  * @copyright 2013–2016 DivanDesign {@link http://www.DivanDesign.biz }
  */
 
-function mm_ddReadonly($fields = '', $roles = '', $templates = ''){
+function mm_ddReadonly(
+	$fields = '',
+	$roles = '',
+	$templates = ''
+){
 	global $modx, $mm_fields, $mm_current_page;
 	$e = &$modx->Event;
 	
@@ -44,7 +53,10 @@ function mm_ddReadonly($fields = '', $roles = '', $templates = ''){
 		$resultFields = array();
 		
 		//Если что-то оплучили
-		if (is_array($tvs) && count($tvs) > 0){
+		if (
+			is_array($tvs) &&
+			count($tvs) > 0
+		){
 			$tvsNames = array();
 			
 			//Пробежимся, переделаем под удобный нам формат
@@ -53,7 +65,12 @@ function mm_ddReadonly($fields = '', $roles = '', $templates = ''){
 			}
 			
 			//Получаем значения TV
-			$tvs = $modx->db->makeArray($modx->db->select('value,tmplvarid AS id', ddTools::$tables['site_tmplvar_contentvalues'], 'contentid='.$docId.' AND tmplvarid IN '.makeSqlList(array_keys($tvsNames))));
+			$tvs = $modx->db->makeArray($modx->db->select(
+				'value,tmplvarid AS id',
+				ddTools::$tables['site_tmplvar_contentvalues'],
+				'contentid='.$docId.' AND tmplvarid IN '.makeSqlList(array_keys($tvsNames))
+			));
+			
 			//Если что-то нашлось
 			if (count($tvs) > 0){
 				//Пробежимся
@@ -70,7 +87,10 @@ function mm_ddReadonly($fields = '', $roles = '', $templates = ''){
 		//Перебираем поля
 		foreach ($fields as $key => $val){
 			//Если такого поля нет или это TV
-			if (!isset($mm_fields[$val]) || $mm_fields[$val]['tv'] == 1){
+			if (
+				!isset($mm_fields[$val]) ||
+				$mm_fields[$val]['tv'] == 1
+			){
 				//Снесём
 				unset($fields[$key]);
 			}
@@ -78,7 +98,12 @@ function mm_ddReadonly($fields = '', $roles = '', $templates = ''){
 		
 		if (count($fields) > 0){
 			//Получаем значения необходимых полей
-			$fields = $modx->db->getRow($modx->db->select(implode(',', $fields), ddTools::$tables['site_content'], 'id='.$docId));
+			$fields = $modx->db->getRow($modx->db->select(
+				implode(',', $fields),
+				ddTools::$tables['site_content'],
+				'id='.$docId
+			));
+			
 			//Переберём
 			foreach($fields as $key => $val){
 				if ($val != ''){
@@ -101,7 +126,11 @@ function mm_ddReadonly($fields = '', $roles = '', $templates = ''){
 		$docId = $e->params['id'];
 		
 		//Если данные о текущем документе есть
-		if (is_array($_SESSION['mm_ddReadonly']) && is_array($_SESSION['mm_ddReadonly'][$docId]) && count($_SESSION['mm_ddReadonly'][$docId]) > 0){
+		if (
+			is_array($_SESSION['mm_ddReadonly']) &&
+			is_array($_SESSION['mm_ddReadonly'][$docId]) &&
+			count($_SESSION['mm_ddReadonly'][$docId]) > 0
+		){
 			//Обновляем данные документа в соответствии с тем, что было раньше
 			ddTools::updateDocument($docId, $_SESSION['mm_ddReadonly'][$docId]);
 			
@@ -114,12 +143,18 @@ function mm_ddReadonly($fields = '', $roles = '', $templates = ''){
 		$tvs = tplUseTvs($mm_current_page['template'], $fields);
 		
 		//Если что-то оплучили
-		if (is_array($tvs) && count($tvs) > 0){
+		if (
+			is_array($tvs) &&
+			count($tvs) > 0
+		){
 			$tvIds = array();
 			foreach ($tvs as $val){$tvIds[] = $val['id'];}
 			
 			//Удаляем значение TV для данного документа
-			$modx->db->delete(ddTools::$tables['site_tmplvar_contentvalues'], '`contentid` = '.$e->params['new_id'].' AND `tmplvarid` IN('.implode(',', $tvIds).')');
+			$modx->db->delete(
+				ddTools::$tables['site_tmplvar_contentvalues'],
+				'`contentid` = '.$e->params['new_id'].' AND `tmplvarid` IN('.implode(',', $tvIds).')'
+			);
 		}
 	//При рендере документа
 	}else if ($e->name == 'OnDocFormRender'){
